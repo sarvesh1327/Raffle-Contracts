984003f2c92563ebcb3951b283706eaf96fddb99
Sarvesh Agarwal <sarveshagl1327@gmail.com>
2024-05-16 12:02:08 +0530

    added enterRaffle tests

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -8,6 +8,9 @@ import {Raffle} from "../../src/Raffle.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract RaffleTest is Test {
+    // events
+    event RaffleEntered(address indexed player);
+
     Raffle public raffle;
     HelperConfig helperConfig;
     uint256 entranceFee;
@@ -16,31 +19,50 @@ contract RaffleTest is Test {
     bytes32 keyHash;
     uint64 subscriptionId;
     uint32 callbackGasLimit;
+    address linkTokenAddress;
     address public PLAYER = makeAddr("Player");
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
 
     function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.run();
-        (entranceFee, interval, vrfCoordinator, keyHash, subscriptionId, callbackGasLimit) =
+        (entranceFee, interval, vrfCoordinator, keyHash, subscriptionId, callbackGasLimit, linkTokenAddress) =
             helperConfig.activeNetworkConfig();
         vm.deal(PLAYER, STARTING_USER_BALANCE);
     }
 
     function testRaffleInitializeWithRaffleOpenState() external view {
-        assert(raffle.getRaffleState()== Raffle.RaffleState.OPEN);
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
     }
 
-    function testRaffleRevertsWhenNotEnoughEntranceFeeIsPaid() external{
+    function testRaffleRevertsWhenNotEnoughEntranceFeeIsPaid() external {
         vm.prank(PLAYER);
         vm.expectRevert(Raffle.Raffle__NotEnoughEntranceFee.selector);
         raffle.enterRaffle();
     }
 
-    function testRaffleRecordsPlayerWhenTheyEnter() external{
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
         vm.prank(PLAYER);
         raffle.enterRaffle{value: 0.02 ether}();
         assertEq(raffle.getTotalPlayers(), 1);
-        assert(raffle.getRafflePlayer(0)==PLAYER);
+        assert(raffle.getRafflePlayer(0) == PLAYER);
+    }
+
+    function testEmitEventOnEntranceOfPlayer() public {
+        vm.prank(PLAYER);
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit RaffleEntered(PLAYER);
+        raffle.enterRaffle{value: 0.02 ether}();
+    }
+
+    function testCantEnterRaffleWhenRaffleIsInCalculatingState() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: 0.02 ether}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep();
+        vm.expectRevert(Raffle.Raffle__NotOpen.selector);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: 0.02 ether}();
     }
 }
